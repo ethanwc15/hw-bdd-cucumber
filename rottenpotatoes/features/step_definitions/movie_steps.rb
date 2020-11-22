540c7005d7df210855b2dc1f1686bc40c413a092
Ethan Curley <ethan.curley@mnsu.edu>
2020-11-22 19:58:22 +0000

    Most tests passing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    
+    #Add each movie to the DB, specifically the Movie table
+    Movie.create(movie)
   end
-  fail "Unimplemented"
+  # fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +21,11 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  # Using RegEx to insert corresponding movies to their correct position within the string
+  e1String = e1.to_s
+  e2String = e2.to_s
+  expect page.body =~ /#{e1String}.+#{e2String}/m
+  # fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +36,21 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  # need to .delete or else there will be space between ratings_ insertRatingHere
+  rating_list.delete(' ').split(',').each do |movieRating|
+      #if checked... go through checked steps, else go through unchecked step...
+      if !uncheck
+          steps %Q{Given I check "ratings_#{movieRating}"}
+      else
+          steps %Q{And I uncheck "ratings_#{movieRating}"}
+      end
+  end        
+  #fail "Unimplemented"
 end
 
-Then /I should see all the movies/ do
+Then /I should see all the movies/ do |n_seeds|
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #Grabbing the movies from the table body, which is where all the movies lay
+  numberOfMovies = page.all('table#movies tbody tr').count
+  #fail "Unimplemented"
 end
